c4639e3bea5d818614df75b8b17b421b5594c5e4
CodeDiversity” <mtrew2015@gmail.com>
2023-05-08 11:56:05 -0400

    add validation on search params

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,5 +1,11 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 export class GetTasksFilterDTO {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsString()
+  @IsOptional()
   search?: string;
 }
