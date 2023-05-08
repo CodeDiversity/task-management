d5f889a2e7dcaada50c5d272729ae73d4622c6e0
CodeDiversity” <mtrew2015@gmail.com>
2023-05-08 11:47:36 -0400

    add pipe validation for task update

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,6 +12,7 @@ import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 import { CreateTaskDTO } from './dto/create-task.dto';
 import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDTO } from './dto/update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -44,8 +45,9 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: Task['status'],
+    @Body() updateTaskStatusDTO: UpdateTaskStatusDTO,
   ): Task {
+    const { status } = updateTaskStatusDTO;
     return this.tasksService.updateTaskStatus(id, status);
   }
 }
